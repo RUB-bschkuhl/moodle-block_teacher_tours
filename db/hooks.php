de13db24141be69f12ecde1ff4f2a01167feff83
Christian Wolters <christian.wolters@uni-luebeck.de>
2025-09-03 11:30:50 +0200

    WIP reuse external admin page

---
+++ db/hooks.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hook callbacks for Teacher Tours
+ *
+ * @package    block_teacher_tours
+ * @copyright  2025 Christian Wolters <christian.wolters@uni-luebeck.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$callbacks = [
+    [
+        'hook' => core\hook\navigation\secondary_extend::class,
+        'callback' => [\block_teacher_tours\local\hook_callbacks::class, 'secondary_extend'],
+        'priority' => 500,
+    ],
+];
