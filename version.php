37cf074de8773380ceb12b3e5a43e5d603bcc513
RUB-bschkuhl <135697714+RUB-bschkuhl@users.noreply.github.com>
2025-12-19 09:05:28 +0100

    Update version.php

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->component = 'block_teacher_tours';
 $plugin->version = 2025112801;
 $plugin->requires = 2025041400; // Moodle 5.0 .
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->supported = [500, 500]; // Moodle 5.0 .
+$plugin->supported = [405, 500]; // Moodle 5.0 .
